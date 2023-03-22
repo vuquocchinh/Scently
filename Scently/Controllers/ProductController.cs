a681547a67a1e961f06262bae5d4642ec71e0557
vuquochinh <chinhvu2002@gmail.com>
2023-03-23 02:04:42 +0700

    new

---
+++ Scently/Controllers/ProductController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Scently.Models;
+
+namespace Scently.Controllers
+{
+    public class ProductController : Controller
+    {
+        ScentlyDataContext data = new ScentlyDataContext();
+
+        // GET: Product
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 5;
+            int pageNum = page ?? 1;
+
+            var Sanpham = from sp in data.SanPhams
+                          orderby sp.idSP
+                          select sp;
+
+            return View(Sanpham.ToPagedList(pageNum, pageSize));
+        }
+
+        //Page List MVC
+
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            var product = data.SanPhams.FirstOrDefault(p => p.idSP == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(SanPham product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    data.SanPhams.InsertOnSubmit(product);
+                    data.SubmitChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Error while creating product.");
+            }
+            return View(product);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var product = data.SanPhams.FirstOrDefault(p => p.idSP == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, SanPham product)
+        {
+            try
+            {
+                var existingProduct = data.SanPhams.FirstOrDefault(p => p.idSP == id);
+                if (existingProduct == null)
+                {
+                    return HttpNotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    existingProduct.tenSP = product.tenSP;
+                    existingProduct.giaSP = product.giaSP;
+                    existingProduct.soLuongSP = product.soLuongSP;
+                    existingProduct.hinhAnhSP = product.hinhAnhSP;
+                    existingProduct.thongTinSP = product.thongTinSP;
+                    existingProduct.ngayCapNhat = product.ngayCapNhat;
+                    existingProduct.idDM = product.idDM;
+                    existingProduct.sale = product.sale;
+                    existingProduct.status = product.status;
+
+                    data.SubmitChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Error while updating product.");
+            }
+            return View(product);
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var product = data.SanPhams.FirstOrDefault(p => p.idSP == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, SanPham product)
+        {
+            try
+            {
+                var existingProduct = data.SanPhams.FirstOrDefault(p => p.idSP == id);
+                if (existingProduct == null)
+                {
+                    return HttpNotFound();
+                }
+                data.SanPhams.DeleteOnSubmit(existingProduct);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Error while deleting product.");
+            }
+            return View(product);
+        }
+    }
+}
---
+++ Scently/Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scently.Models;
+
+namespace Scently.Controllers
+{
+    public class UserController : Controller
+    {
+        ScentlyDataContext data = new ScentlyDataContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Login
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var loginUser = data.TaiKhoans.Where(x => x.taiKhoan1 == user.TaiKhoan && x.matKhau == user.MatKhau).FirstOrDefault();
+
+            if (loginUser != null)
+            {
+                // Đăng nhập thành công, thực hiện lưu session và chuyển hướng đến trang chủ
+                Session["taiKhoan"] = loginUser.taiKhoan1;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Đăng nhập không thành công, trả về thông báo lỗi
+                ViewBag.Error = "Tài khoản hoặc mật khẩu không đúng";
+                return View();
+            }
+        }
+
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View(new TaiKhoan());
+        }
+
+        // POST: Register
+        [HttpPost]
+        public ActionResult Register(TaiKhoan user)
+        {
+            if (ModelState.IsValid)
+            {
+                // Kiểm tra tài khoản đã tồn tại chưa
+                var existingUser = data.TaiKhoans.FirstOrDefault(x => x.taiKhoan1 == user.taiKhoan1);
+                if (existingUser != null)
+                {
+                    ViewBag.Error = "Tài khoản đã tồn tại";
+                    return View();
+                }
+
+                // Thêm mới tài khoản
+                data.TaiKhoans.InsertOnSubmit(user);
+                data.SubmitChanges();
+
+                // Đăng ký thành công, thực hiện lưu session và chuyển hướng đến trang chủ
+                Session["taiKhoan"] = user.taiKhoan1;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Dữ liệu không hợp lệ, trả về thông báo lỗi
+                ViewBag.Error = "Dữ liệu không hợp lệ";
+                return View();
+            }
+        }
+    }
+}
